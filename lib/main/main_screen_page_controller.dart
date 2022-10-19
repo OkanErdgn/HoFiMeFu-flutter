eb3737251b99586889c24ef636ee533dd922769c
OkanErdgn <14okanerdoganfb1999@gmail.com>
2022-10-19 21:09:02 +0300

    main screen page controller added
    main screen content is now can be swiped left or right

---
+++ lib/main/main_screen_page_controller.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/widgets.dart';
+
+import 'main_screen_content.dart';
+
+class MainPageView extends StatefulWidget {
+  @override
+  MainPageViewState createState() => MainPageViewState();
+}
+
+class MainPageViewState extends State<MainPageView> {
+
+  PageController _controller = PageController(
+    initialPage: 1,
+  );
+
+   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      scrollDirection: Axis.horizontal,
+      children: [
+        MainScreenContent(), //TODO Left Screen ?
+        MainScreenContent(), //TODO Main Screen 
+        MainScreenContent(), //TODO Friends/Chat screen
+      ],
+    );
+  }
+}
